034cf8318544c184cba08a27899c1e53daf1d4aa
ilkerogtc <ilkerogtc@gmail.com>
2022-02-25 16:20:15 +0300

    2.hafta bitiyo

--- 2.hafta/file.js
+++ 2.hafta/file.js
@@ -3,5 +3,18 @@ const fs = require("fs");
 const path = require("path");
 
 
-fs.mkdirSync(path.resolve(__dirname,"dir"));
+fs.mkdirSync(path.resolve(__dirname, "dir"));
 
+
+const writeFileAsync = async (path, data) => {
+    return new Promise((resolve, reject) => fs.writeFile(path, data, (err) => {
+        if (err) return reject(err.message);
+        return resolve();
+    }));
+}
+
+writeFileAsync(path.resolve(__dirname, "asdsa.txt"), 'data')
+    .then(() => fs.appendFile(path.resolve(__dirname, "asdsa.txt", "aşsdlaşsldşsl")));
+
+
+// promise konusuna bak
